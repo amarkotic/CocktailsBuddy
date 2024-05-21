98fec56d026c6db5b54914e9c4e3c12f13d63e0c
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-05-21 18:15:45 +0200

    T01: Add main modules

---
+++ Networking/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Networking",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "Networking",
+            targets: ["Networking"]),
+    ],
+    targets: [
+        .target(
+            name: "Networking"),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]),
+    ]
+)
