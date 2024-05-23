8d0242e1cd9f204e4cd8ec64ea3d8cdae36f9e90
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-05-23 18:10:33 +0200

    T04: DividerView

---
+++ CoreUI/Sources/CoreUI/Components/Divider/DividerView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public struct DividerView: View {
+
+    public init() {}
+
+    public var body: some View {
+        Divider()
+            .foregroundStyle(Color.gray)
+    }
+
+}
