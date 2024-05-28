2045573612e1da3542dbcd81929d929cde0cbc7e
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-05-29 00:44:41 +0200

    T10: App fonts

---
+++ CoreUI/Sources/CoreUI/Fonts/Font+Extensions.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public extension Font {
+
+    static var headerPrimary: Font {
+        .system(size: 18, weight: .bold, design: .default)
+    }
+
+    static var headerSecondary: Font {
+        .system(size: 16, weight: .bold, design: .default)
+    }
+
+    static var bodyPrimary: Font {
+        .system(size: 16, weight: .regular, design: .default)
+    }
+
+    static var bodySecondary: Font {
+        .system(size: 12, weight: .regular, design: .default)
+    }
+
+}
